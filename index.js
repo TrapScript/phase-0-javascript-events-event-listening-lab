ef909c32f46b9624ddfd97e17daab66ec71798c4
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-07-09 22:27:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
-}
+    const mainElement = document.getElementById("main");
+    const inputElement = document.getElementById("input");
+    const divElements = document.querySelectorAll("div");
+    
+    mainElement.addEventListener("click", function() {
+        console.log("Main element clicked!");
+    });
+
+    inputElement.addEventListener("input", function() {
+        console.log("Input value changed!");
+      });
+    
+      divElements.forEach(function(div) {
+        div.addEventListener("mouseover", function() {
+          console.log("Mouse over a div!");
+        });
+      });
+    }
